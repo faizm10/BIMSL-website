a6d13df5cbe7bde7734c61aa11ada237f4d62693
faizm10 <faizmustansar10@gmail.com>
2025-11-23 11:03:06 -0500

    removed drizzle

--- frontend/app/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error('DATABASE_URL environment variable is not set');
-}
-
-const db = drizzle(databaseUrl);
